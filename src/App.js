92b0410257c5f75791dd77bf747b664d96f1422f
jamsheerply <jamsheerpayyoli@gmail.com>
2024-02-22 17:22:08 +0300

    home page ready

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContentext";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Requests";
 
 function Home() {
   return (
     <div>
       <Main />
+      <Row rowID="1" title="Up Comping" fetchURL={requests.requestUpcoming} />
+      <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID="3" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
     </div>
   );
 }
